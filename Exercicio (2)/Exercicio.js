1d9c8f3f52303fe6377c65e01f1014620bff2dd5
Enioth <82185824+Eniote@users.noreply.github.com>
2025-01-17 23:35:37 -0300

    Exercicio 2 parcial 2

--- Exercicio (2)/Exercicio.js
+++ Exercicio (2)/Exercicio.js
@@ -1,5 +1,3 @@
-document.getElementById("execute").addEventListener("click", executeFunction);
-
 function factorial(num) {
     if (num === 0) {
         return 1;
@@ -10,22 +8,24 @@ function factorial(num) {
 function executeFunction(num) {
     let promEl = document.getElementById("prompt");
     let respEl = document.getElementById("resposta");
-    let formLabEl = document.getElementById("formLabel");
-    let formImpEl = document.getElementById("num");
 
+    if (isNaN(num)) {
+        promEl.textContent = "Você digitou uma string, digite um número";
+        return;
+    }
 
-    do {
-        if (num < 0) {
-            promEl.textContent = "Digite um número maior que 0";
-        }else{
-            respEl.textContent = "O fatorial de " + num + " é " + factorial(num);
-            promEl.textContent = "Gostaria de calcular um novo fatorial?";
-            formLabEl.textContent = "Responda S para sim e N para não:";
-            formImpEl.setAttribute("type", "String");
-        }
-    } while (false);
-
-    return;
+    if (num < 0) {
+        promEl.textContent = "Digite um número maior que 0";
+    } else {
+        respEl.textContent = "O fatorial de " + num + " é " + factorial(num);
+        promEl.textContent = "Gostaria de calcular um novo fatorial? Responda S para sim e N para não:";
+    }
+    
 }
 
 
+document.getElementById("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    let num = parseInt(document.getElementById("num").value);
+    executeFunction(num);
+});
